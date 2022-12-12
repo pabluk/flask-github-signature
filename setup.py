4ec6b7dc3a33589e455c0c569cb33706b98ec211
Pablo Seminario <pablo@seminar.io>
2022-12-12 20:51:55 +0100

    Update Python's classifiers

--- setup.py
+++ setup.py
@@ -19,12 +19,16 @@ setup(
     packages=["flask_github_signature"],
     classifiers=[
         "Development Status :: 4 - Beta",
+        "Framework :: Flask",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Libraries",
     ],
     project_urls={
